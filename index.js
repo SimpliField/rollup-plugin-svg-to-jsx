e9a2763c28b5407674e344aa34ad18ea6f68e8f8
Georges Plancke <georges.plancke@simplifield.com>
2019-08-06 12:54:57 +0000

    modify for stencil

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
-'use strict'
+const svgToJsx = require('svg-to-jsx');
+const MagicString = require('magic-string');
+const { createFilter } = require('rollup-pluginutils');
+const { basename } = require('path');
+const { readFileSync } = require('fs');
 
-var svgToJsx = require('svg-to-jsx')
-var MagicString = require('magic-string')
-var createFilter = require('rollup-pluginutils').createFilter
+module.exports = function svgToJsx(options = {}) {
+  const filter = createFilter(options.include || '**/*.svg', options.exclude);
 
-module.exports = function (options) {
-  options = options || {}
-  var filter = createFilter(options.include || '**/*.svg', options.exclude)
   return {
-    transform: function sourceToCode (code, id) {
-      if (!filter(id)) return null
+    transform: (code, id) => {
+      if (!filter(id)) return null;
 
-      var s = new MagicString(code)
-      return svgToJsx(code).then(function (jsx) {
-        var result = 'import React from "react";\nexport default props => ' +
-          jsx.replace(/^<svg/, '<svg {...props}') + ';\n'
-        s.overwrite(0, code.length, result)
+      const name = toName(id);
+      const content = readFileSync(id, 'utf-8').toString();
+
+      return new Promise((res, rej) => {
+        res(content);
+      }).then((jsx) => {
+        const result = toExport(jsx, name);
+
+        console.log(result);
+
         return {
-          code: s.toString(),
-          map: s.generateMap({ hires: true })
-        }
+          code: result,
+        };
       })
     }
   }
+}
+
+function toExport(jsx, name = 'Icon') {
+  return `
+import { h, FunctionalComponent } from '@stencil/core;
+
+export ${name}:FunctionalComponent () => ${jsx}
+`;
+}
+
+function toName(id) {
+  const base = basename(id, '.svg');
+  const name = base.split('-').filter(s => isNaN(s)).join('');
+
+  return capitalize(name);
+}
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
